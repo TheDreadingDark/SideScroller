fba1ba305572b71057d93e6041e275edc038412c
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-05-02 13:08:28 -0600

    Added code for Weapons.

--- SideScroller/Controller/SpaceGame.cs
+++ SideScroller/Controller/SpaceGame.cs
@@ -50,6 +50,13 @@ namespace SideScroller.Controller
 		// A random number generator
 		private Random random;
 
+		private Texture2D projectileTexture;
+		private List<Projectile> projectiles;
+
+		// The rate of fire of the player laser
+		private TimeSpan fireTime;
+		private TimeSpan previousFireTime;
+
 		public SpaceGame()
 		{
 			graphics = new GraphicsDeviceManager(this);
@@ -87,6 +94,11 @@ namespace SideScroller.Controller
 			// Initialize our random number generator
 			random = new Random();
 
+			projectiles = new List<Projectile>();
+
+			// Set the laser to fire every quarter second
+			fireTime = TimeSpan.FromSeconds(.15f);
+
 			base.Initialize();
 		}
 
@@ -116,6 +128,8 @@ namespace SideScroller.Controller
 			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 
 			enemyTexture = Content.Load<Texture2D>("Animation/mineAnimation");
+
+			projectileTexture = Content.Load<Texture2D>("Texture/laser");
 		}
 
 		/// <summary>
@@ -156,6 +170,9 @@ namespace SideScroller.Controller
 			// Update the collision
 			UpdateCollision();
 
+			// Update the projectiles
+			UpdateProjectiles();
+
 			base.Update(gameTime);
 		}
 
@@ -181,6 +198,12 @@ namespace SideScroller.Controller
 			for (int i = 0; i<enemies.Count; i++)
 			{
 			enemies[i].Draw(spriteBatch);
+			}
+
+			// Draw the Projectiles
+			for (int i = 0; i<projectiles.Count; i++)
+			{
+			    projectiles[i].Draw(spriteBatch);
 			}
 
 			// Draw the Player 
@@ -220,6 +243,17 @@ namespace SideScroller.Controller
 			// Make sure that the player does not go out of bounds
 			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
 			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+
+			// Fire only every interval we set as the fireTime
+			if (gameTime.TotalGameTime - previousFireTime > fireTime)
+			{
+			    // Reset our current time
+			    previousFireTime = gameTime.TotalGameTime;
+
+				// Add the projectile, but add it to the front and center of the player
+				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+			}
+
 		}
 
 		private void AddEnemy()
@@ -297,6 +331,47 @@ namespace SideScroller.Controller
 					{
 						player.Active = false;
 					}
+				}
+			}
+
+			// Projectile vs Enemy Collision
+			for (int i = 0; i<projectiles.Count; i++)
+			{
+				for (int j = 0; j<enemies.Count; j++)
+				{
+					// Create the rectangles we need to determine if we collided with each other
+				    rectangle1 = new Rectangle((int)projectiles[i].Position.X - projectiles[i].Width / 2, (int) projectiles[i].Position.Y - 
+					projectiles[i].Height / 2, projectiles[i].Width, projectiles[i].Height);
+
+				    rectangle2 = new Rectangle((int)enemies[j].Position.X - enemies[j].Width / 2, (int) enemies[j].Position.Y - enemies[j].Height / 2, enemies[j].Width, enemies[j].Height);
+
+				    // Determine if the two objects collided with each other
+				    if (rectangle1.Intersects(rectangle2))
+				    {
+				    	enemies[j].Health -= projectiles[i].Damage;
+				    	projectiles[i].Active = false;
+				 	}
+				}
+			}
+		}
+
+		private void AddProjectile(Vector2 position)
+		{
+			Projectile projectile = new Projectile();
+			projectile.Initialize(GraphicsDevice.Viewport, projectileTexture, position);
+			projectiles.Add(projectile);
+		}
+
+		private void UpdateProjectiles()
+		{
+			// Update the Projectiles
+			for (int i = projectiles.Count - 1; i >= 0; i--)
+			{
+				projectiles[i].Update();
+
+				if (projectiles[i].Active == false)
+				{
+					projectiles.RemoveAt(i);
 				}
 			}
 		}
